c05271e5bddbac5824d349251ede73ac79e97e4b
shantanu more <shantanumore10@gmail.com>
2020-07-28 20:50:44 +0530

    added files in  day1

---
+++ day1/6boolean.js
@@ -0,0 +1,21 @@
+let bnumber = 6555;
+
+console.log(Boolean(bnumber));
+
+let example1 = false;
+let example2 = true;
+let example3 = null;
+let example4 = undefined;
+let example5 = '';
+let example6 = NaN;
+let example7 = -5;
+let example8 = 0;
+
+console.log(Boolean(example1));
+console.log(Boolean(example2));
+console.log(Boolean(example3));
+console.log(Boolean(example4));
+console.log(Boolean(example5));
+console.log(Boolean(example6));
+console.log(Boolean(example7));
+console.log(Boolean(example8));
