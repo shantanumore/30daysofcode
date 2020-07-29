22c803714529112fc25292d6f65126eb1ee38d26
shantanu more <shantanumore10@gmail.com>
2020-07-29 22:45:14 +0530

    updated index and script

--- day2/script.js
+++ day2/script.js
@@ -1,4 +1,25 @@
-const heading1 = document.getElementById("heading");
+// const heading1 = document.getElementById("heading");
 
-console.log(heading1);
+// console.log(heading1);
 
+// const list = document.getElementById("list");
+
+// console.log(list.textContent);
+
+// console.log(list.innerHTML);
+
+const list = document.querySelector("#list");
+
+console.log(list);
+
+const li = document.querySelector('li');
+
+console.log(li);
+
+const lis = document.querySelectorAll('li');
+
+console.log(lis);
+
+const listitems = document.querySelectorAll('.list-items');
+
+console.log(listitems);
