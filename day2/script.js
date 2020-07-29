0afb4fa47a01df31097bdc3437d3970b3c397e83
shantanu more <shantanumore10@gmail.com>
2020-07-29 23:53:03 +0530

    updated some changes

--- day2/script.js
+++ day2/script.js
@@ -8,18 +8,57 @@
 
 // console.log(list.innerHTML);
 
-const list = document.querySelector("#list");
+// const list = document.querySelector("#list");
 
-console.log(list);
+// console.log(list);
 
-const li = document.querySelector('li');
+// const li = document.querySelector('li');
 
-console.log(li);
+// console.log(li);
 
-const lis = document.querySelectorAll('li');
+// const lis = document.querySelectorAll('li');
 
-console.log(lis);
+// console.log(lis);
 
-const listitems = document.querySelectorAll('.list-items');
+// const listitems = document.querySelectorAll('.list-items');
 
-console.log(listitems);
+// console.log(listitems);
+
+
+// const heading = document.getElementById('heading');
+
+// heading.style.color = 'red';
+// heading.style.backgroundColor = 'cyan'
+
+// const lis = document.querySelectorAll('li');
+// console.log(lis);
+
+// lis[1].style.color='blue';
+
+// for (let index = 0; index < lis.length; index++) {
+//     lis[index].style.backgroundColor = 'yellow';
+    
+// }
+
+// lis[0].style.cssText ="background-color: coral;color: white;font-size:25px";
+
+// const heading = document.getElementById(".heading");
+// const btn = document.getElementById(".btn");
+
+// // btn.onclick = ()=> {
+// //      console.log("Clicked!");
+// // };
+
+// btn.onmouseover= () =>{
+//   heading.style.cssText = "background-color: brown; color: white";
+// };
+
+
+// const clickButton = () =>{
+//     console.log("clicked!");
+// };
+
+// btn.addEventListener('click', e =>{
+//     heading.style.cssText = "background-color: brown; color: white";
+// });
+
