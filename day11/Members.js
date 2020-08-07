4d324e03ef949cefcbc5c0e50dd93c5527e5768b
shantanu more <shantanumore10@gmail.com>
2020-08-07 14:22:19 +0530

    day 11 commit

---
+++ day11/Members.js
@@ -0,0 +1,24 @@
+const members=[ 
+    {
+        id : '1',
+        name: 'shantanu',
+        email: 'shantanumore10@gmail.com',
+        status: 'active'
+    },
+    {
+        id : '2',
+        name: 'yash',
+        email: 'goswami3@gmail.com',
+        status: 'inactive'
+    },
+    {
+        id : '3',
+        name: 'ritik',
+        email: 'ritik34@gmail.com',
+        status: 'active'
+    }
+
+];
+
+
+module.exports=members;
